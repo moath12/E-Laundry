3743c57ef4a136ecf1300b5c9d55a5a0a3fad365
MOATH <moahalmr@gmail.com>
2017-02-26 19:22:03 +0300

    i delet class

--- app/src/main/java/mnm/e_laundry/Admin.java
+++ app/src/main/java/mnm/e_laundry/Admin.java
@@ -8,5 +8,4 @@ public class Admin {
     public String admEmail;
     public String password;
     public String adminName;
-
 }
--- app/src/main/java/mnm/e_laundry/tt.java
+++
@@ -1,8 +0,0 @@
-package mnm.e_laundry;
-
-/**
- * Created by MOATH on 26 فبر، 2017 م.
- */
-public class tt {
-    public int i;
-}
